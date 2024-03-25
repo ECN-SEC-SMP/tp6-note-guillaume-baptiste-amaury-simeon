8a3f521f6ed78c7ef641b01fa2dd8b03e9d2e480
Guillaume de Maricourt <ahz45bny@gmail.com>
2024-03-25 09:01:50 +0100

    arch:create board

---
+++ board/board.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+// System headers
+#include <vector>
+
+// Project headers
+#include "tiles/tile.hpp"
+#include "../../constans.h"
+
+using namespace std;
+
+class Board
+{
+private:
+    vector<Player> _players = vector<Player>();
+    vector<Tile> _tiles;
+
+    int _availableHouses = kDefaultGameHouses;
+    int _availableHotels = kDefaultGameHotels;
+
+    void addOneTile(Tile &tile);
+    void addOnePlayer(Player &player);
+
+public:
+    // Constructors
+    Board();
+
+    // Getters
+    vector<Player> getPlayers(void);
+    vector<Tile> getTiles(void);
+    int getAvailableHouses(void);
+    int getAvailableHotels(void);
+
+    // Setters
+    void setPlayers(vector<Player>);
+    void setTiles(vector<Tile>);
+    void setAvailableHouses(int availableHouses);
+    void setAvailableHotels(int availableHotels);
+
+    // Methods
+    void initBoard(void);
+    void registerPlayers(void);
+
+    void playOneGlobalTurn(void);
+    void playOnePlayerTurn(Player &player);
+};
